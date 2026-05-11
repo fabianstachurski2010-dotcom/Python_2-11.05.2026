d7acfdd9ae7f7e68e0e194b49ad9ac01a535aee6
Fabian Stachurski <fabianstachurski2010@gmail.com>
2026-05-11 17:06:58 +0200

    Added subtraction

--- calc.py
+++ calc.py
@@ -1,6 +1,9 @@
 def add(a, b):
     return a + b
+def substract(a, b):
+    return a - b
 
 if __name__ == "__main__":
     print("Welcome to the Python Calc!")
     print(f"2 + 3 = {add(2, 3)}")
+    print(f"2 - 3 = {substract(2, 3)}")
