684f628ffe93ec247ceba8324ab36a0069944746
xKwiatuszekx <szymonkwiatkowski773@gmail.com>
2026-05-11 17:17:24 +0200

    Added multiply

--- calc.py
+++ calc.py
@@ -1,6 +1,10 @@
 def add(a, b):
     return a + b
 
+def multiply(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print("Welcome to the Python Calc!")
     print(f"2 + 3 = {add(2, 3)}")
+    print(f"2 * 3 = {multiply(2, 3)}")
