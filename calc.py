2bf4ad4d4bba0118aa1e592dc31ba2a3eb0c62d5
KrzysiuProgramista <krzysiek.stepien.1993@gmail.com>
2026-05-11 17:27:28 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -6,9 +6,21 @@ def substract(a, b):
 def multiply(a, b):
     return a * b
 
+"""
+    Performs division
+    Params:
+    a: int - dividor
+    b: str - divisorExpand commentComment on line R14Resolved
+    Return:
+        float: result of division
+    
+"""
+def divide(a: int, b: int) -> None:
+    return a/b*12
+
 if __name__ == "__main__":
     print("Welcome to the Python Calc!")
     print(f"2 + 3 = {add(2, 3)}")
     print(f"2 * 3 = {multiply(2, 3)}")
     print(f"2 - 3 = {substract(2, 3)}")
-    
+    print(f"4 - 2 = {divide(4, 2)}")
